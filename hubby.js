56697b6f4bb88b9a631a7561ea727c108ea208c0
r2997790 <nayrsllew@gmail.com>
2020-05-08 23:12:04 +0300

    Add files via upload

--- hubby.js
+++ hubby.js
@@ -3,17 +3,18 @@ console.log("boottour  loaded....");
 var tour = new Tour({
   steps: [
     {
-      element: "#hplogo",
-      title: "Title of my step Hubby",
+      element: "#root > div > div.footer > div.content.center",
+      title: "Title of my step 1",
       content: "Content of my step 1",
       placement: "auto"
     },
     {
-      element: "#footer",
+      element: "#root > div > div.main > div > h1",
       title: "Title of my step 2",
       content: "Content of my step 2",
       placement: "auto"
     }
+
     // {
     //   element: "#myDiv > h3",
     //   title: "Title of my step 3",
